9089b99c735dbdd0bf18bd2fe6e676989c826526
s-shultz <s.shultz@wustl.edu>
2020-11-13 17:07:50 -0800

    week 7, part 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
 jQuery(function() {
 
-	$('#my_button').click(function () {
+	let my_switch = false;
+
+	$('#p1').click(function () {
+
+	if (my_switch == false) {
+		my_switch = true;
+
+		$(this).html('Congratulations!');
+
+		$('#my_image').attr('src', 'images/nothired.gif');
+
+
+	} else {
+		my_switch = false;
+
+		$(this).html('Condolences');
+
+			$('#my_image').attr('src', 'images/employment.gif');
+
+
+
+
+	}
+
+
+
 	});
 
 });
